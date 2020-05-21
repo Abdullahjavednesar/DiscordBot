ad375b22307c9bc92d42d7facaa96f49a827e5a2
Abdullah Javed Nesar <abdullah.nesar@innovaccer.com>
2020-05-21 12:30:08 +0530

    init commit

---
+++ bot.py
@@ -0,0 +1,64 @@
+import discord
+import requests
+import re
+from bs4 import BeautifulSoup
+import sqlite3
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        db = sqlite3.connect('history.sqlite')
+        cursor = db.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS history(
+            user_id TEXT,
+            search_phrase TEXT
+        )
+        ''')
+        print('Bot online, logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'hi':
+            await message.channel.send('hey')
+
+        functionality_type = message.content[0:7]
+        url_list = []
+        if functionality_type == '!google':
+            results = 5
+            search = message.content.replace('!google ', '')
+            db = sqlite3.connect('history.sqlite')
+            cursor = db.cursor()
+            sql = ("INSERT INTO history(user_id, search_phrase) VALUES(?,?)")
+            val = (message.author.id, search)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+            page = requests.get(f"https://www.google.com/search?q={search}&num={results+1}")
+            soup = BeautifulSoup(page.content)
+            links = soup.findAll("a")
+            for link in links :
+                link_href = link.get('href')
+                if "url?q=" in link_href and not "webcache" in link_href:
+                    url_list.append(link.get('href').split("?q=")[1].split("&sa=U")[0])
+        for url in url_list[0:5]:
+            await message.channel.send(url)
+        
+        if functionality_type == '!recent':
+            check_history = message.content.replace('!recent ', '')
+            db = sqlite3.connect('history.sqlite')
+            cursor = db.cursor()
+            cursor.execute(f"SELECT DISTINCT search_phrase FROM history WHERE search_phrase LIKE '%{check_history}%' AND user_id = {message.author.id}")
+            result = cursor.fetchall()
+            if result == []:
+                await message.channel.send("No result found in history :(")
+            else:
+                for phrase in result:
+                    await message.channel.send(phrase[0])
+
+        
+
+client = MyClient()
+client.run('NzEyNTY3NzI2Mjc0NTEwODU5.XsTcdg.F3qvI5xfDl_IWE5kZQE3Dtm7zik')
